f679d6eaa24e5bc1f23162bd52fb1dd385085c70
Jan <janekrembikowski@gmail.com>
2023-07-17 14:25:36 +0200

    fix api

--- ML/main.py
+++ ML/main.py
@@ -1,8 +1,11 @@
 import numpy as np
 import joblib
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
 
 svm_model = joblib.load("MLmodel.joblib")
 
@@ -15,11 +18,17 @@ def index():
 @app.route("/get-prediction", methods=["POST"])
 def predict_if_patient_has_lung_cancer():
     try:
+        prediction = 0
+        print(prediction)
         user_data = request.json["user_data"]
         new_patient = np.array(user_data)
         new_patient = new_patient.reshape(1, -1)
+        new_patient = new_patient.astype(str)
+        print(new_patient)
 
         prediction = svm_model.predict(new_patient)
+
+        print(prediction)
 
         result = {
             "prediction": prediction.tolist()
